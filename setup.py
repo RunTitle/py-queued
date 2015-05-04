88ffc4f05e4dfb9bf9b9acc8fda0c0385d5d0c6c
Stephen Durham <sdurham@runtitle.com>
2015-05-04 16:28:38 -0500

    Setting up testing framework for app.

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ setup(
     name="Queued",
     version="0.1",
     packages=find_packages(),
-
+    install_requires=[
+        'boto>=2.38.0'
+    ],
+    test_suite='queued.tests',
     # metadata for upload to PyPI
     author="Stephen Durham",
     author_email="sdurham@runtitle.com",
     description="Amazon SQS and SNS integration library for python",
     license="PSF",
     keywords="Amazon SQS SNS",
-    url="http://example.com/HelloWorld/",   # project home page, if any
+    url="http://example.com/py-queued/",   # project home page, if any
 )
